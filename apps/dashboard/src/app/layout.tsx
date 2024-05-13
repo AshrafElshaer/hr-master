b6daa205eac997e2d019a8ca45f5b54e03d217fb
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-05-13 14:33:32 -0500

    adding metadata to layout

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -1,15 +1,46 @@
 import "@hr-toolkit/ui/globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+
 import { Providers } from "@/components/providers";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 
-const inter = Inter({ subsets: ["latin"] });
+const baseUrl = "https://hr-toolkit-dashboard.vercel.app";
 
 export const metadata: Metadata = {
-	title: "HR Toolkit",
-	description: "A toolkit for HR professionals",
+	metadataBase: new URL(baseUrl),
+	title: {
+		default: "HR Toolkit",
+		template: "%s | HR Toolkit",
+	},
+	description:
+		"HR Toolkit is a collection of tools to help you manage HR processes.",
+	openGraph: {
+		title: "HR Toolkit | Manage your HR processes smarter",
+		description: "This is my portfolio.",
+		url: baseUrl,
+		siteName: "HR Toolkit | Manage your HR processes smarter",
+		locale: "en_US",
+		type: "website",
+	},
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	},
+};
+
+export const viewport = {
+	themeColor: [
+		{ media: "(prefers-color-scheme: light)" },
+		{ media: "(prefers-color-scheme: dark)" },
+	],
 };
 
 export default function RootLayout({
