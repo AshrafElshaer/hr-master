501256792de4bc56fa4081b169baab4b24b9e04f
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-05-17 15:15:47 -0500

    chore: Refactor getUser function in user queries

--- packages/supabase/src/queries/user/index.ts
+++ packages/supabase/src/queries/user/index.ts
@@ -3,11 +3,7 @@ import type { SupabaseClient } from "../../types";
 export async function getUser(supabase: SupabaseClient) {
   const {
     data: { user },
-    error,
   } = await supabase.auth.getUser();
-  if (error) {
-    throw new Error(error.message);
-  }
 
   return user;
 }
