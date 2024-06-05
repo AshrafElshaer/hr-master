cd9dbc8d65876191ed261c132f44cbe7ef54d097
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-06-05 13:19:26 -0500

    chore: Refactor documents navigation component folder size

--- apps/dashboard/src/app/(root)/employees/[id]/documents/navigation.tsx
+++ apps/dashboard/src/app/(root)/employees/[id]/documents/navigation.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { DEFAULT_STORAGE_FOLDERS } from "@/constants/default-storage-files";
 import Link from "next/link";
 import { IoIosFolderOpen } from "react-icons/io";
@@ -18,28 +18,29 @@ export default function DocumentsNavigation({ employeeId }: Props) {
 	return (
 		<section className="w-full flex items-center gap-4 flex-wrap">
 			{DEFAULT_STORAGE_FOLDERS.map((folder) => {
-				
 				const isActivePath = pathname.endsWith(folder.path);
-				return(
-				<Link
-					key={folder.path}
-					className={cn(
-						buttonVariants({
-							variant: "ghost",
-						}),
-						"flex flex-col items-center hover:bg-background",
-						isActivePath && "text-foreground"
-					)}
-					href={`/employees/${employeeId}/documents/${folder.path}`}
-				>
-					<IoIosFolderOpen size={60} />
-					{folder.name}
-				</Link>
-			)})}
+				return (
+					<Link
+						key={folder.path}
+						className={cn(
+							buttonVariants({
+								variant: "ghost",
+							}),
+							"flex flex-col items-center hover:bg-background",
+							isActivePath && "text-foreground",
+						)}
+						href={`/employees/${employeeId}/documents/${folder.path}`}
+					>
+						<IoIosFolderOpen className="w-10 h-10 sm:w-14 sm:h-14" />
+						{folder.name}
+					</Link>
+				);
+			})}
 			<button
-			type="button"
-			className="flex flex-col items-center text-foreground/70 text-sm">
-				<FolderPlus size={60} />
+				type="button"
+				className="flex flex-col items-center text-foreground/70 text-sm"
+			>
+				<FolderPlus className="w-10 h-10 sm:w-14 sm:h-14" />
 				Add Folder
 			</button>
 		</section>
