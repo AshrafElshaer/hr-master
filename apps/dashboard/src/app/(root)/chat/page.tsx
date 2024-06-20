55a799b18e97a088d19baeb08fbe3a1bb9c1b3ad
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-06-20 11:59:03 -0500

    add project & chat pages

---
+++ apps/dashboard/src/app/(root)/chat/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ChatPage() {
+  return (
+    <main className="flex flex-col items-center justify-center h-full ">
+      ChatPage
+    </main>
+  );
+}
--- apps/dashboard/src/constants/sidebar-navigations.tsx
+++ apps/dashboard/src/constants/sidebar-navigations.tsx
@@ -2,53 +2,65 @@ import { Clock, Home, Users } from "lucide-react";
 import { TbCalendarPause } from "react-icons/tb";
 import { HiOutlineBanknotes } from "react-icons/hi2";
 import { LuLayoutGrid } from "react-icons/lu";
+import { IoChatbubblesOutline } from "react-icons/io5";
+import { PiKanban } from "react-icons/pi";
 
 export const sidebarNavigations = [
-  {
-    title: "Dashboard",
-    path: "/",
-    icon: <Home size={18} />,
-  },
-  {
-    title: "Employees",
-    path: "/employees",
-    icon: <Users size={18} />,
-  },
-  {
-    title: "Departments",
-    path: "/departments",
-    icon: <LuLayoutGrid size={18} />,
-  },
-  {
-    title: "Attendance",
-    path: "/attendance",
-    icon: <Clock size={18} />,
-  },
-  {
-    title: "Time Off",
-    path: "/time-off",
-    icon: <TbCalendarPause size={18} />,
-  },
-  {
-    title: "Payroll",
-    path: "/payroll",
-    icon: <HiOutlineBanknotes size={18} />,
-  },
+	{
+		title: "Dashboard",
+		path: "/",
+		icon: <Home size={18} />,
+	},
+	{
+		title: "Employees",
+		path: "/employees",
+		icon: <Users size={18} />,
+	},
+	{
+		title: "Departments",
+		path: "/departments",
+		icon: <LuLayoutGrid size={18} />,
+	},
+	{
+		title: "Attendance",
+		path: "/attendance",
+		icon: <Clock size={18} />,
+	},
+	{
+		title: "Time Off",
+		path: "/time-off",
+		icon: <TbCalendarPause size={18} />,
+	},
+	{
+		title: "Payroll",
+		path: "/payroll",
+		icon: <HiOutlineBanknotes size={18} />,
+	},
+	{
+		title: "Projects",
+		path: "/projects",
+		icon: <PiKanban size={18} />,
+	},
+	{
+		title: "Chat",
+		path: "/chat",
+		icon: <IoChatbubblesOutline size={18} />,
+	},
 ];
 
 export const roleBasedNavigations = (role: string) => {
-  switch (role) {
-    case "owner":
-      return sidebarNavigations;
-    case "manager":
-      return sidebarNavigations.filter(
-        (route) => route.path !== "/departments",
-      );
-    case "employee":
-      return sidebarNavigations.filter(
-        (route) => route.path !== "/departments" && route.path !== "/employees",
-      );
-    default:
-      return [];
-  }
+	switch (role) {
+		case "owner":
+			return sidebarNavigations;
+		case "manager":
+			return sidebarNavigations.filter(
+				(route) => route.path !== "/departments",
+			);
+		case "employee":
+			return sidebarNavigations.filter(
+				(route) => route.path !== "/departments" && route.path !== "/employees",
+			);
+		default:
+			return [];
+	}
 };
