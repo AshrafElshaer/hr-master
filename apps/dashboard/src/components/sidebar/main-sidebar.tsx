e3fcffae204570b700fd576629f14590a1054a95
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-06-20 12:22:03 -0500

    seperator main siddbar

--- apps/dashboard/src/components/sidebar/main-sidebar.tsx
+++ apps/dashboard/src/components/sidebar/main-sidebar.tsx
@@ -1,62 +1,67 @@
 "use client";
-import {
-  roleBasedNavigations,
-  sidebarNavigations,
-} from "@/constants/sidebar-navigations";
-import type { ReactSetState } from "@/types";
-import { createClient } from "@hr-toolkit/supabase/client";
-import type { User } from "@hr-toolkit/supabase/types";
-import { getUser } from "@hr-toolkit/supabase/user-queries";
-import { buttonVariants } from "@hr-toolkit/ui/button";
-import { cn } from "@hr-toolkit/ui/utils";
-import { useQuery } from "@tanstack/react-query";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
 import React from "react";
+import { usePathname } from "next/navigation";
+import { roleBasedNavigations } from "@/constants/sidebar-navigations";
+import { cn } from "@hr-toolkit/ui/utils";
+
+import type { ReactSetState } from "@/types";
+import type { User } from "@hr-toolkit/supabase/types";
+
+import { buttonVariants } from "@hr-toolkit/ui/button";
+import { Separator } from "@hr-toolkit/ui/separator";
+import Link from "next/link";
 
 function MainSidebar({
-  setIsMobileOpen,
-  currentUser,
+	setIsMobileOpen,
+	currentUser,
 }: {
-  setIsMobileOpen?: ReactSetState<boolean>;
-  currentUser: User;
+	setIsMobileOpen?: ReactSetState<boolean>;
+	currentUser: User;
 }) {
-  const pathname = usePathname();
+	const pathname = usePathname();
 
-  return (
-    <nav className="w-full h-full">
-      <ul className="flex flex-col items-start justify-start h-full gap-1 p-2">
-        {roleBasedNavigations(currentUser.role ?? "").map((route) => {
-          const isActivePath =
-            pathname === route.path ||
-            route.path === pathname.split("/").slice(0, 2).join("/");
+	return (
+		<nav className="w-full h-full">
+			<ul className="flex flex-col items-start justify-start h-full gap-1 p-2">
+				{roleBasedNavigations(currentUser.role ?? "").map((route, idx) => {
+					const isActivePath =
+						pathname === route.path ||
+						route.path === pathname.split("/").slice(0, 2).join("/");
 
-          return (
-            <li key={route.path} className="w-full justify-start">
-              <Link
-                href={route.path}
-                className={cn(
-                  buttonVariants({
-                    variant: isActivePath ? "secondary" : "ghost",
-                    className: "w-full justify-start gap-2 relative",
-                  }),
-                )}
-                onClick={() => {
-                  setIsMobileOpen?.(false);
-                }}
-              >
-                {route.icon}
-                {route.title}
-                {isActivePath ? (
-                  <div className="absolute right-0 top-1 bottom-1 w-[3px] rounded-l bg-primary" />
-                ) : null}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
-  );
+					return (
+						<>
+							{idx === 7 && (
+								<Separator
+									key={`${route.path}-seperatr`}
+									className="w-full my-4"
+								/>
+							)}
+							<li key={route.path} className="w-full justify-start">
+								<Link
+									href={route.path}
+									className={cn(
+										buttonVariants({
+											variant: isActivePath ? "secondary" : "ghost",
+											className: "w-full justify-start gap-2 relative",
+										}),
+									)}
+									onClick={() => {
+										setIsMobileOpen?.(false);
+									}}
+								>
+									{route.icon}
+									{route.title}
+									{isActivePath ? (
+										<div className="absolute right-0 top-1 bottom-1 w-[3px] rounded-l bg-primary" />
+									) : null}
+								</Link>
+							</li>
+						</>
+					);
+				})}
+			</ul>
+		</nav>
+	);
 }
 
 export default MainSidebar;
