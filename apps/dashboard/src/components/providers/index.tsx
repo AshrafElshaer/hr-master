d6ca0d43fa1da6a5be6a9b8bafb0361a87b3b17e
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-06-20 15:56:37 -0500

    add analytics

--- apps/dashboard/src/components/providers/index.tsx
+++ apps/dashboard/src/components/providers/index.tsx
@@ -4,21 +4,23 @@ import ReactQuery from "./react-query";
 import { ThemeProvider } from "./theme";
 import { Toaster } from "@hr-toolkit/ui/sonner";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import { env } from "@hr-toolkit/env";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <ReactQuery>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-        <Toaster richColors position="top-right" duration={5000} />
-        {env.NEXT_PUBLIC_NODE_ENV === "production" && <SpeedInsights />}
-      </ThemeProvider>
-    </ReactQuery>
-  );
+	return (
+		<ReactQuery>
+			<ThemeProvider
+				attribute="class"
+				defaultTheme="system"
+				enableSystem
+				disableTransitionOnChange
+			>
+				{children}
+				<Toaster richColors position="top-right" duration={5000} />
+				<Analytics />
+				{env.NEXT_PUBLIC_NODE_ENV === "production" && <SpeedInsights />}
+			</ThemeProvider>
+		</ReactQuery>
+	);
 }
