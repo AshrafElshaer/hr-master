487c4c2cdd60a605ff71e6d61fa3a7552f5561be
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-06-21 20:51:33 -0500

    clock in out skeleton

--- apps/dashboard/src/app/(root)/(home)/page.tsx
+++ apps/dashboard/src/app/(root)/(home)/page.tsx
@@ -1,12 +1,18 @@
+import { Suspense } from "react";
 import ClockInOut from "./_components/clock-in-out";
+import { Skeleton } from "@hr-toolkit/ui/skeleton";
 
 export default function IndexPage() {
 	return (
 		<main className="flex flex-col gap-4 h-full p-4">
-			 <h2 className="md:text-xl">
-				Dashboard
-			 </h2>
-			<ClockInOut />
+			<h2 className="md:text-xl">Dashboard</h2>
+			<Suspense fallback={<ClockInOutSkeleton />}>
+				<ClockInOut />
+			</Suspense>
 		</main>
 	);
+}
+
+function ClockInOutSkeleton() {
+	return <Skeleton className="ml-auto w-full sm:w-80 " />;
 }
