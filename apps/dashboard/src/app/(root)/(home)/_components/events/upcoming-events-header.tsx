efff641ebc396ae82d86e30690204e98f766aac0
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-06-28 10:31:48 -0500

    refactor: Add user role check for displaying create event button

--- apps/dashboard/src/app/(root)/(home)/_components/events/upcoming-events-header.tsx
+++ apps/dashboard/src/app/(root)/(home)/_components/events/upcoming-events-header.tsx
@@ -8,8 +8,22 @@ import { DatePickerWithRange } from "@hr-toolkit/ui/date-range-picker";
 import { Separator } from "@hr-toolkit/ui/separator";
 import CreateEvent from "./create-events";
 import { addDays, format } from "date-fns";
+import { createClient } from "@hr-toolkit/supabase/client";
+import { useQuery } from "@tanstack/react-query";
+import { getUser } from "@hr-toolkit/supabase/user-queries";
 
 export default function UpcomingEventsHeader() {
+	const { data: user } = useQuery({
+		queryKey: ["user"],
+		queryFn: async () => {
+			const supabase = createClient();
+			const { error, user } = await getUser(supabase);
+			if (error) {
+				throw Error(error.message);
+			}
+			return user;
+		},
+	});
 	const router = useRouter();
 	const [date, setDate] = React.useState<DateRange>({
 		from: new Date(),
@@ -54,8 +68,12 @@ export default function UpcomingEventsHeader() {
 					numberOfMonths={1}
 					className=" w-64"
 				/>
-				<Separator orientation="vertical" />
-				<CreateEvent />
+				{user?.role !== "employee" ? (
+					<>
+						<Separator orientation="vertical" />
+						<CreateEvent />
+					</>
+				) : null}
 			</div>
 		</div>
 	);
